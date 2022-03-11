800866cfb508ab099465c4ee93eb1589567d0d12
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-03-11 11:44:04 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="discord-ext-components",
+    version="1.0.0",
+    author="Saroniii",
+    packages=["discord.ext.components"],
+    description="Easy to use components for py-cord",
+)
