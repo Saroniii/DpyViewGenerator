ea0a081a8b2719fe5f7fb0b117f77d0a5784f63a
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-03-12 10:48:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="dpy-bot-utils",
     version="2.0.2",
     author="Saroniii",
-    packages=["dpy_bot_utils", "dpy_bot_utils.components"],
+    packages=find_packages(where="dpy_bot_utils"),
     description="Easy to use components for py-cord",
 )
