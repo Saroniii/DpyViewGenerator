744d1775ff2217f5426ec84011e88e3c96060d09
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-03-12 10:12:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="discord-ext-components",
     version="1.0.2",
     author="Saroniii",
-    packages=["discord, discord.ext, discord.ext.components"],
+    packages=["discord.ext.components"],
     description="Easy to use components for py-cord",
 )
