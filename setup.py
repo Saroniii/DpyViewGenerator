0d8ab2da12c2d0a9e7a9d725604d0b699b8471e5
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-03-12 10:21:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="discord-ext-components",
     version="1.0.2",
     author="Saroniii",
-    packages=["discord.ext.components"],
+    packages=["dpy_bot_utils", "dpy_bot_utils.components"],
     description="Easy to use components for py-cord",
 )
