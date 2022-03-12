007e67807563a08383938212c078510f865ceb59
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-03-12 11:53:31 +0900

    Delete dpy_bot_utils directory

--- dpy_bot_utils/components/__init__.py
+++
@@ -1,2 +0,0 @@
-from .components import Modal, InputText
-from .view import ViewGenerator, Button, Select, SelectOption, SelectTriggerType, ViewUsedBehaviorType, RespondTargetType, ComponentsUtils, ButtonStyle
