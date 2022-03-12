9b547fa1451ad0d2624bcad665b8ef3a3b0e7cd2
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-03-12 10:23:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="discord-ext-components",
+    name="dpy-bot-utils",
     version="1.0.2",
     author="Saroniii",
     packages=["dpy_bot_utils", "dpy_bot_utils.components"],
