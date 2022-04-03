69bddb579830cfa9988899429aa4cd6085f5e768
Saroniii <54314408+Saroniii@users.noreply.github.com>
2022-04-03 12:58:02 +0900

    Update __init__.py

--- dpy_bot_utils/components/__init__.py
+++ dpy_bot_utils/components/__init__.py
@@ -1,2 +1,2 @@
-from .components import Modal, InputText
+from .components import Modal, TextInput
 from .view import ViewGenerator, Button, Select, SelectOption, SelectTriggerType, ViewUsedBehaviorType, RespondTargetType, ComponentsUtils, ButtonStyle
