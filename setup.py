a131ef3a840634c062fd75974497ed1e2e828a30
Saroniii <54314408+Saroniii@users.noreply.github.com>
2025-09-24 16:18:33 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dpy-bot-utils",
+    name="dpy_bot_utils",
     version="2.0.7",
     author="Saroniii",
     packages=["dpy_bot_utils", "dpy_bot_utils.components"],
     description="Easy to use components for discord.py",
 )
+
